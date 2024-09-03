5f114c38b028a67c158bf82abfb84a933b163df5
necco-neko <nekonecconeko@gmail.com>
2024-09-03 16:42:25 +0900

    使用する画像の読み込みとタイトルを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import tilesImages from './tilesImages';
 
 const App: React.FC = () => {
   return (
-    <div>あああ</div>
+    <div className='container'>
+      <h1 className='title'>麻雀点数計算</h1>
+      <div className='option-container'>
+      </div>
+    </div>
   )
 }
 
