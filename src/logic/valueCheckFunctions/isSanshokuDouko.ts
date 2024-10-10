16c0d5d08722a100cfd467a664da4d055cf97971
necco-neko <nekonecconeko@gmail.com>
2024-10-10 20:12:04 +0900

    テスト用consoleの削除

--- src/logic/valueCheckFunctions/isSanshokuDouko.ts
+++ src/logic/valueCheckFunctions/isSanshokuDouko.ts
@@ -12,9 +12,7 @@ const isSanshokuDouko = (structure: string[][]): boolean => {
         //groupが刻子であることと数牌であることを確認
         if (group.length === 3 && group[0] === group[1] && !jihaiList.includes(group[0])) {
             const suit = group[0][0];
-            console.log("suit:" + suit);
             const number = parseInt(group[0][1]);
-            console.log("number:" + number);
 
             //萬子・筒子・索子ごとに刻子の数字を配列に追加
             triplets[suit].push(number);
