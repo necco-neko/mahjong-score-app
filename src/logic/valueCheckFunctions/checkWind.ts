06b2a29aae6bef9721d7ea8ac1a2730ab978994f
necco-neko <nekonecconeko@gmail.com>
2024-10-10 20:12:33 +0900

    風牌のアルゴリズムの修正

--- src/logic/valueCheckFunctions/checkWind.ts
+++ src/logic/valueCheckFunctions/checkWind.ts
@@ -1,13 +1,23 @@
 const checkWind = (jikaze: string, bakaze: string, tileCount: { [key: string]: number }): string[] => {
     const yakuList: string[] = [];
 
+    //風は日本語入力なので牌名(英語)に変換
+    const windTranslate: { [key: string]: string } = {
+        "東": "east",
+        "南": "south",
+        "西": "west",
+        "北": "north"
+    };
+    const jikazeKey = windTranslate[jikaze];
+    const bakazeKey = windTranslate[bakaze];
+
     //自風の確認
-    if (tileCount[jikaze] >= 3) {
+    if (tileCount[jikazeKey] >= 3) {
         yakuList.push(`門風牌(${jikaze})`);
     }
 
     //場風の確認
-    if (tileCount[bakaze] >= 3) {
+    if (tileCount[bakazeKey] >= 3) {
         yakuList.push(`荘風牌(${bakaze})`);
     }
 
