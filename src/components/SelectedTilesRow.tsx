b1162ec6141ed46a301887620405460aa3cae13b
necco-neko <nekonecconeko@gmail.com>
2025-03-11 15:35:53 +0900

    牌画像（裏面）のパスを修正

--- src/components/SelectedTilesRow.tsx
+++ src/components/SelectedTilesRow.tsx
@@ -14,7 +14,7 @@ interface SelectedTilesRowProps {
 
 export const SelectedTilesRow: React.FC<SelectedTilesRowProps> = ({ ponCount, chiiCount, kanCount, hasCalled, selectedTiles, typeOfKan, setTypeOfKan }) => {
     const nakiCount = ponCount + chiiCount + kanCount
-    const backTileSrc = '/tupai2_1/p_bk_1.gif';
+    const backTileSrc = `${process.env.PUBLIC_URL}/tupai2_1/p_bk_1.gif`;
 
     const getDisplayedTiles = (kanType: boolean) => {
         //明カンならそのまま
