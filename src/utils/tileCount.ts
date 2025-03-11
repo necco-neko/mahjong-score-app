247a9efdd0d9fdfdad72def3fce3eaf6ec3feeae
necco-neko <nekonecconeko@gmail.com>
2025-03-11 17:08:03 +0900

    Updates

--- src/utils/tileCount.ts
+++
@@ -1,9 +0,0 @@
-const tileCount = (tiles: string[]) => {
-    const tileCount = tiles.reduce((countMap, tile) => {
-        countMap[tile] = (countMap[tile] || 0) + 1;
-        return countMap;
-    }, {} as { [key: string]: number });
-    return tileCount;
-}
-
-export default tileCount;
